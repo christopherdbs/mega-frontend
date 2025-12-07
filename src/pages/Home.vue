68fc8dd6b3ca89f5b5445d4ada52e52d6b9d5d7f
christopherdbs <32715148+christopherdbs@users.noreply.github.com>
2025-12-07 20:18:41 +0100

    feat: add navigation

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,17 +1,9 @@
 <script setup>
-    import Navbar from '../components/Navbar.vue';
-    import Highlights from '../components/Highlights.vue';
-    import Footer from '../components/Footer.vue';
+import Highlights from "../components/Highlights.vue";
 </script>
 
-
 <template>
-    <Navbar /> 
     <Highlights />
-    <Footer />
 </template>
 
-
-<style>
-
-</style>
+<style></style>

eca09d5ebc0bebe521b023fb5a9fae8a50f88a93
christopherdbs <christopherdb77@gmail.com>
2025-12-14 10:50:23 +0100

    feat: add sign in page

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,13 @@ import Home from "../pages/Home.vue";
 import Catalog from "../pages/Catalog.vue";
 import NotFound from "../pages/NotFound.vue";
 import Cart from "../pages/Cart.vue";
+import Login from "../pages/Login.vue";
+
 const routes = [
     { path: "/", component: Home },
     { path: "/catalog", component: Catalog },
     { path: "/cart", component: Cart },
-    { path: "/connexion", component: Cart },
+    { path: "/login", component: Login },
     { path: "/game/:id", component: () => import("../pages/GameDetail.vue") },
     { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound },
 ];
